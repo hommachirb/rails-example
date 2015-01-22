0ea1a47773e5138e19169cc5e325004b8e5928e6
ogom <ogom@hotmail.co.jp>
2015-01-23 00:07:22 +0900

    Fixed application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def bootswatch_alert_types(alert_type)
+    case alert_type.to_sym
+    when :info then
+      'alert-info'
+    when :notice, :success then
+      'alert-success'
+    when :alert, :error then
+      'alert-danger'
+    else
+      'alert-warning'
+    end
+  end
 end
