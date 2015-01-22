a979b4d214eed9a0520afed88e0aade04bc3fa88
ogom <ogom@hotmail.co.jp>
2015-01-22 23:28:10 +0900

    Fixed session controller

--- app/controllers/user/session_controller.rb
+++ app/controllers/user/session_controller.rb
@@ -1,7 +1,39 @@
 class User::SessionController < ApplicationController
+  skip_before_action :authenticate, except: :show
+
+  # GET /
   def show
   end
 
+  # GET /user/sign_in
   def new
   end
+
+  # POST /user/sign_in
+  def create
+    user = User.sign_in(user_params)
+    if user.present?
+      session[:user_id] = user.id
+      if session[:user_redirect_url].present?
+        redirect_url = session[:user_redirect_url]
+        session.delete :user_redirect_url
+      end
+      redirect_to redirect_url ||= root_path, notice: 'Signed in successfully.'
+    else
+      flash[:warning] = 'Invalid username or password.'
+      render :new
+    end
+  end
+
+  # DELETE /user/sign_out
+  def destroy
+    session.delete :user_id
+    redirect_to root_path, notice: 'You need to sign in before continuing.'
+  end
+
+  private
+    def user_params
+      params.permit(:username, :password)
+    end
+  # end private
 end
