6ec74cf15959ca94c015a4d584f2c7bd5c1f8944
ogom <ogom@hotmail.co.jp>
2015-01-22 23:19:03 +0900

    Fixed user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  serialize :roles, Array
+
+  class << self
+    def sign_in(params)
+      find_by(username: params[:username]).try(:authenticate, params[:password])
+    end
+  end
 end
